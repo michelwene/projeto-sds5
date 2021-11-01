2245337aea68e874fa555113e415d066c4a37bc9
Michel Wene <michelwene@hotmail.com>
2021-11-01 12:27:55 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
+import 'assets/css/style.css';
 
 ReactDOM.render(
   <React.StrictMode>
