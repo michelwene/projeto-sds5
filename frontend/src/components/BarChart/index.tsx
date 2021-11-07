5103faa63a912030108fcdb60fbb662ef7b55b4f
Michel Wene <michelwene@hotmail.com>
2021-11-07 10:01:06 -0300

    Barchart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,34 +1,71 @@
-import Chart from 'react-apexcharts';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import { SaleSuccess } from "types/sale";
+import { round } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
+type SeriesData = {
+  name: string;
+  data: number[];
+};
+
+type ChartData = {
+  labels: {
+    categories: string[];
+  };
+  series: SeriesData[];
+};
 
 const BarChart = () => {
-    const options = {
-        plotOptions: {
-            bar: {
-                horizontal: true,
-            }
-        },
-    };
-    
-    const mockData = {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: {
+      categories: [],
+    },
+    series: [
+      {
+        name: "",
+        data: [],
+      },
+    ],
+  });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then((response) => {
+      const data = response.data as SaleSuccess[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => round((100.0 * x.deals) / x.visited, 1));
+
+      setChartData({
         labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+          categories: myLabels,
         },
         series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-            }
-        ]
-    };    
+          {
+            name: "% Success",
+            data: mySeries,
+          },
+        ],
+      });
+    });
+  }, []);
 
-    return (
-        <Chart 
-            options={{...options, xaxis: mockData.labels}}
-            series={mockData.series}
-            type='bar'
-            height='240'
-        />
-    );
+  const options = {
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
   };
-  
-  export default BarChart;
+
+  return (
+    <Chart
+      options={{ ...options, xaxis: chartData.labels }}
+      series={chartData.series}
+      type="bar"
+      height="240"
+    />
+  );
+};
+
+export default BarChart;
